87b98cfcfca4738e61811919cf17607cd73f8eb2
Prashant Kumar <prash.k20@gmail.com>
2022-06-20 01:30:26 -0700

    Deleted old classes from git
    deleted old classes from git, added change in main.cpp

--- main.cpp
+++ main.cpp
@@ -12,21 +12,22 @@
 // Date June 16th 2022
 //
 
-#include "GameClass.h"
+#include "PlayHexGameClass.h"
+#include "ProcessHexBoardClass.h"
 
 //main hex game program parameters
-bool GameClass::play_game_ = true;              //make this true to only test print hex board function
-int GameClass::board_size_ = 11;                 // 5 for testing, 11 for production
-unsigned int GameClass::num_of_simulations_ = 1000;
-bool GameClass::debug_mode_ = false;            //when true, simulation win-loss-ratio will be printed, can be enabled at runtime by inputting board size with a suffix 'd'
+bool PlayHexGameClass::play_game_ = true;              //make this true to only test print hex board function
+int ProcessHexBoardClass::board_size_ = 11;                 // 5 for testing, 11 for production
+unsigned int ProcessHexBoardClass::num_of_simulations_ = 1000;
+bool PlayHexGameClass::debug_mode_ = false;            //when true, simulation win-loss-ratio will be printed, can be enabled at runtime by inputting board size with a suffix 'd'
 
 int main()
 {
     //initialize hex game
-    GameClass hex_game_object;
+    PlayHexGameClass hex_game_object;
 
     //play hex game or test print hex board
-    if (GameClass::play_game_)
+    if (PlayHexGameClass::play_game_)
         hex_game_object.RunGame();
     else
         hex_game_object.PrintHexBoard();
